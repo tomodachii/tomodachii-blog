0cc9ee9ae54ba2fbf8e5113f6b7bf59218d5980a
Hoang Do <tuanhoang2do2311@gmail.com>
2025-02-13 08:28:34 +0000

    Updated site

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const mode = localStorage.getItem("mode") || "";
 const toggle = document.querySelector(".toggle");
 const body = document.querySelector("body");
+const sun = document.querySelector("#sun");
+const moon = document.querySelector("#moon");
 
 document.body.className = mode;
 
 toggle.addEventListener("click", () => {
     localStorage.setItem("mode", mode === "light" ? "" : "light");
     body.classList.toggle("light");
+
+    if (sun.style.display === "none") {
+        sun.style.display = "block";
+        moon.style.display = "none";
+    } else {
+        sun.style.display = "none";
+        moon.style.display = "block";
+    }
 })
